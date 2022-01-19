74442581c80c1ebe506d73570a81f4c124d99e3b
mozgovoy <60607081+mozgovoy@users.noreply.github.com>
2022-01-19 23:57:05 +0300

    Update settings.py

--- mchs/settings.py
+++ mchs/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mchs',
         'USER' : 'postgres',
-        'PASSWORD' : 'nikitam2003',
+        'PASSWORD' : '',
         'HOST' : '127.0.0.1',
         'PORT' : '5432',
     }
